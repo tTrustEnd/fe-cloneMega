ab52bcb85fb4a9aa2df2d51dda6372f8fc7c4d39
Truong <banoianh1x@gmail.com>
2023-06-30 08:20:28 +0700

    a -> navlink

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,7 @@ import YTB from '../../../src/public/img-youtube.png'
 import FB from '../../../src/public/img-face.png'
 import './index.scss'
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 const Header = () => {
     const user = useSelector((state: any) => state.user.account.user)
     return (
@@ -13,13 +13,13 @@ const Header = () => {
             </div>
             <div className="content-header">
                 <div className="content-top">
-                    <a href="/gioi-thieu"><ul>GIỚI THIỆU</ul></a>
-                    <a href="/su-kien"><ul>SỰ KIỆN</ul></a>
-                    <a href="/dich-vu"><ul>DỊCH VỤ</ul></a>
-                    <a href="/tuyen-dung"><ul>TUYỂN DỤNG</ul></a>
-                    <a href="/faqs"><ul>FAQS</ul></a>
-                    <a href="/lien-he"><ul>LIÊN HỆ</ul></a>
-                    <a href="/megaPlus"> <ul>MEGA+</ul></a>
+                    <NavLink to="/gioi-thieu"><ul>GIỚI THIỆU</ul></NavLink>
+                    <NavLink to="/su-kien"><ul>SỰ KIỆN</ul></NavLink>
+                    <NavLink to="/dich-vu"><ul>DỊCH VỤ</ul></NavLink>
+                    <NavLink to="/tuyen-dung"><ul>TUYỂN DỤNG</ul></NavLink>
+                    <NavLink to="/faqs"><ul>FAQS</ul></NavLink>
+                    <NavLink to="/lien-he"><ul>LIÊN HỆ</ul></NavLink>
+                    <NavLink to="/megaPlus"> <ul>MEGA+</ul></NavLink>
                     {!user && <a href="/register"><div> ĐĂNG KÝ</div> </a>}
 
                     {!user && <a href="/login"> <button style={{ fontWeight: 600, height: 40 }} className="btn btn-warning">ĐĂNG NHẬP</button></a>}
@@ -43,10 +43,10 @@ const Header = () => {
                 </div>
                 <div className="content-bot">
                     <div>
-                        <a href="/lich-chieu"><b>LỊCH CHIẾU</b></a>
-                        <a href="/phim"><b>PHIM</b></a>
-                        <a href="/rap"><b>RẠP & GIÁ VÉ</b></a>
-                        <a href="/uu-dai"><b>ƯU ĐÃI</b></a>
+                        <NavLink to="/lich-chieu"><b>LỊCH CHIẾU</b></NavLink>
+                        <NavLink to="/phim"><b>PHIM</b></NavLink>
+                        <NavLink to="/rap"><b>RẠP & GIÁ VÉ</b></NavLink>
+                        <NavLink to="/uu-dai"><b>ƯU ĐÃI</b></NavLink>
                     </div>
                     <div style={{ paddingLeft: 50 }}>
                         <a style={{ paddingRight: 25 }} href="https://www.facebook.com/nguyenquang.truong.9237" target="_blank" title="Facebook"><img src={FB} /></a>
